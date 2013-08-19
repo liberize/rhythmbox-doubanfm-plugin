f32cdeb05e0d5982bf56d130299d568ffb1b185f
liberize <liberize@gmail.com>
2013-08-19 23:59:23 +0800

    change icon dir to /usr/share/icons

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 schema_dir="/usr/share/glib-2.0/schemas"
 schema_file="./org.gnome.rhythmbox.plugins.doubanfm.gschema.xml"
-icon_dir="$HOME/.local/share/icons"
+icon_dir="/usr/share/icons"
 icon_file="./doubanfm.png"
 plugin_dir="$HOME/.local/share/rhythmbox/plugins"
 pulgin_folder="$PWD"
@@ -11,10 +11,7 @@ sudo cp "$schema_file" "$schema_dir"
 sudo glib-compile-schemas "$schema_dir"
 
 echo "copying icon file ..."
-if [ ! -d "$icon_dir" ]; then
-	mkdir -p "$icon_dir"
-fi
-cp "$icon_file" "$icon_dir"
+sudo cp "$icon_file" "$icon_dir"
 
 echo "copying plugin folder ..."
 if [ ! -d "$plugin_dir" ]; then
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 schema_file="/usr/share/glib-2.0/schemas/org.gnome.rhythmbox.plugins.doubanfm.gschema.xml"
-icon_file="$HOME/.local/share/icons/doubanfm.png"
+icon_file="/usr/share/icons/doubanfm.png"
 pulgin_folder="$PWD"
 
 echo "removing schema file ..."
@@ -8,7 +8,7 @@ sudo rm -f "$schema_file"
 sudo glib-compile-schemas "$(dirname "$schema_file")"
 
 echo "removing icon file ..."
-rm -f "$icon_file"
+sudo rm -f "$icon_file"
 
 echo "removing plugin folder ..."
 rm -rf "$pulgin_folder"
